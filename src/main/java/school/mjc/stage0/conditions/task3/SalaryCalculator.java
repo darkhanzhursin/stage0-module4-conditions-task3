c213cb58a600c886c5bece0a5fe1c25c2a01901b
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-09-01 00:59:37 +0600

    Task #3. Multiple Conditions

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double updated;
+        if (salary <= 10000 && salary > 0) {
+            updated = salary*0.85;
+            System.out.println(updated);
+        } else if (salary > 10000 && salary <= 20000) {
+            updated = salary*0.82;
+            System.out.println(updated);
+        } else if (salary > 20000) {
+            updated = salary*0.8;
+            System.out.println(updated);
+        } else {
+            System.out.println("wrong input!");
+        }
     }
 }
